a20a73ffcb1abe19be663af7bf4bfee6939cd851
Nicholas M. Synovic <Nicholas.Synovic@GMail.com>
2021-04-10 21:44:31 +0000

    Add code to collect words and number of pages

--- wordList.py
+++ wordList.py
@@ -1,29 +1,37 @@
 import re
 
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 
 
 class WordList:
     def __init__(self) -> None:
         self.merriamWebsterURL = "https://www.merriam-webster.com/browse/dictionary/"
 
-    def getHTML(self, url: str) -> BeautifulSoup:
-        resp = requests.get(url=url).text
+    def getHTML(self, letter: str, page: str) -> BeautifulSoup:
+        resp = requests.get(url=self.merriamWebsterURL + letter + "/" + page).text
         return BeautifulSoup(markup=resp, features="lxml")
 
     def parseHTML(self, html: BeautifulSoup) -> tuple:
-        def getNumberOfPages() -> int:
-            numberOfPagesText = html.find(name="span", attrs={"class": "counters"})
-            print(numberOfPagesText.text)
+        def _getNumberOfPages() -> int:
+            numberOfPagesText = html.find(name="span", attrs={"class": "counters"}).text
+            pages = int(re.findall("[^\D+]\d+", numberOfPagesText)[-1])
+            return pages
 
-        wordSet = set()
-        getNumberOfPages()
+        def _getWords() -> set():
+            words = set()
+            wordsDiv: Tag = html.find(name="div", attrs={"class": "entries"})
+            wordsList = wordsDiv.find_all(name="a")
+            for word in wordsList:
+                words.add(word.text)
+            return words
+
+        return (_getNumberOfPages(), _getWords())
 
     def program(self) -> bool:
         return False
 
 
 w = WordList()
-h = w.getHTML(w.merriamWebsterURL)
+h = w.getHTML("a", "1")
 p = w.parseHTML(h)
