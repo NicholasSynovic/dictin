09b7c081ef035b45b9626980b432a2602d9596cf
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-07-24 13:05:29 +0000

    Add type annotations

--- MerriamWebster.py
+++ MerriamWebster.py
@@ -5,75 +5,80 @@ from bs4 import BeautifulSoup, Tag
 from bs4.element import ResultSet
 from progress.bar import Bar
 from progress.spinner import MoonSpinner
+from requests.models import Response
 
 
 class WordList:
     def __init__(self) -> None:
-        self.merriamWebsterURL = "https://www.merriam-webster.com/browse/dictionary/"
+        self.merriamWebsterURL: str = (
+            "https://www.merriam-webster.com/browse/dictionary/"
+        )
         self.currentURL: str = ""
 
     def getHTML(self, letter: str, page: str = "1") -> BeautifulSoup:
         self.currentURL = self.merriamWebsterURL + letter + "/" + page
-        resp = requests.get(url=self.currentURL).text
+        resp: Response = requests.get(url=self.currentURL).text
         return BeautifulSoup(markup=resp, features="lxml")
 
     def getNumberOfPages(self, html: BeautifulSoup) -> int:
-        numberOfPagesText = html.find(name="span", attrs={"class": "counters"}).text
+        numberOfPagesText: str = html.find(
+            name="span", attrs={"class": "counters"}
+        ).text
         try:
-            pages = int(re.findall("[^\D+]\d+", numberOfPagesText)[-1])
+            pages: int = int(re.findall("[^\D+]\d+", numberOfPagesText)[-1])
         except IndexError:
-            pages = int(re.findall("\d", numberOfPagesText)[-1])
+            pages: int = int(re.findall("\d", numberOfPagesText)[-1])
         return pages
 
     def getWords(self, html: BeautifulSoup) -> list:
-        words = []
+        words: list = []
         wordsDiv: Tag = html.find(name="div", attrs={"class": "entries"})
         wordsList: ResultSet = wordsDiv.find_all(name="a")
+
+        word: str
         for word in wordsList:
             words.append(word.text)
         return words
 
 
 def program() -> bool:
-    words = []
-    mwLetterList = ["0"]
-    mwPageCount = []
+    words: list = []
+    mwLetterList: list = ["0"]
+    mwPageCount: list = []
 
-    with MoonSpinner("Creating Char List... ") as spinner:
-        for unicodeChar in range(97, 123):
-            mwLetterList.append(chr(unicodeChar).lower())
-            spinner.next()
+    unicodeChar: int
+    for unicodeChar in range(97, 123):
+        mwLetterList.append(chr(unicodeChar).lower())
 
-    print()
+    mw: WordList = WordList()
 
-    mw = WordList()
+    with Bar("Getting page numbers for dictionary keys... ", max=27) as bar:
 
-    with Bar("Getting Page Numbers for Dictionary Indicies... ", max=27) as bar:
+        letter: str
         for letter in mwLetterList:
-            firstPage = mw.getHTML(letter=letter)
-            pageCount = mw.getNumberOfPages(firstPage)
+            firstPage: BeautifulSoup = mw.getHTML(letter=letter)
+            pageCount: int = mw.getNumberOfPages(firstPage)
             mwPageCount.append(pageCount)
             bar.next()
 
     mwDict = dict(zip(mwLetterList, mwPageCount))
 
-    print()
-
+    key: str
     for key in mwDict.keys():
         with Bar(
-            "Getting Words Under the Dictionary Index: " + key + "... ",
+            "Getting words listed under the dictionary index: " + key + "... ",
             max=mwDict[key],
         ) as bar:
+            page: int
             for page in range(mwDict[key]):
-                pageString = str(page + 1)
-                html = mw.getHTML(letter=key, page=pageString)
+                pageString: str = str(page + 1)
+                html: BeautifulSoup = mw.getHTML(letter=key, page=pageString)
                 words += mw.getWords(html=html)
                 mwDict[key] = len(words)
                 bar.next()
 
-    print()
-
     with open("output/MerriamWebster_WordList.txt", "w") as wordList:
+        word: str
         for word in words:
             wordList.write(word + "\n")
         print("Wrote word list to file")
