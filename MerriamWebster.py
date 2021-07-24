0e18233714a02010397595cf54e68cade824c431
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-07-24 14:28:52 +0000

    Simplify code

--- MerriamWebster.py
+++ MerriamWebster.py
@@ -7,60 +7,52 @@ from progress.bar import Bar
 from requests.models import Response
 
 
-class WordList:
-    def __init__(self) -> None:
-        self.merriamWebsterURL: str = (
-            "https://www.merriam-webster.com/browse/dictionary/"
-        )
-        self.currentURL: str = ""
+def getHTML(letter: str, page: str = "1") -> BeautifulSoup:
+    resp: Response = requests.get(
+        url="https://www.merriam-webster.com/browse/dictionary/" + letter + "/" + page
+    ).text
+    return BeautifulSoup(markup=resp, features="lxml")
 
-    def getHTML(self, letter: str, page: str = "1") -> BeautifulSoup:
-        self.currentURL = self.merriamWebsterURL + letter + "/" + page
-        resp: Response = requests.get(url=self.currentURL).text
-        return BeautifulSoup(markup=resp, features="lxml")
 
-    def getNumberOfPages(self, html: BeautifulSoup) -> int:
-        numberOfPagesText: str = html.find(
-            name="span", attrs={"class": "counters"}
-        ).text
-        try:
-            pages: int = int(re.findall("[^\D+]\d+", numberOfPagesText)[-1])
-        except IndexError:
-            pages: int = int(re.findall("\d", numberOfPagesText)[-1])
-        return pages
+def getNumberOfPages(html: BeautifulSoup) -> int:
+    numberOfPagesText: str = html.find(name="span", attrs={"class": "counters"}).text
+    try:
+        pages: int = int(re.findall("[^\D+]\d+", numberOfPagesText)[-1])
+    except IndexError:
+        pages: int = int(re.findall("\d", numberOfPagesText)[-1])
+    return pages
 
-    def getWords(self, html: BeautifulSoup) -> list:
-        words: list = []
-        wordsDiv: Tag = html.find(name="div", attrs={"class": "entries"})
-        wordsList: ResultSet = wordsDiv.find_all(name="a")
 
-        word: str
-        for word in wordsList:
-            words.append(word.text)
-        return words
+def getWords(html: BeautifulSoup) -> list:
+    words: list = []
+    wordsDiv: Tag = html.find(name="div", attrs={"class": "entries"})
+    wordsList: ResultSet = wordsDiv.find_all(name="a")
+
+    word: str
+    for word in wordsList:
+        words.append(word.text)
+    return words
 
 
 if __name__ == "__main__":
     words: list = []
-    mwLetterList: list = ["0"]
-    mwPageCount: list = []
+    letterList: list = ["0"]
+    pageCount: list = []
 
     unicodeChar: int
     for unicodeChar in range(97, 123):
-        mwLetterList.append(chr(unicodeChar).lower())
-
-    mw: WordList = WordList()
+        letterList.append(chr(unicodeChar).lower())
 
     with Bar("Getting page numbers for dictionary keys... ", max=27) as bar:
 
         letter: str
-        for letter in mwLetterList:
-            firstPage: BeautifulSoup = mw.getHTML(letter=letter)
-            pageCount: int = mw.getNumberOfPages(firstPage)
-            mwPageCount.append(pageCount)
+        for letter in letterList:
+            firstPage: BeautifulSoup = getHTML(letter=letter)
+            pageCount: int = getNumberOfPages(firstPage)
+            pageCount.append(pageCount)
             bar.next()
 
-    mwDict = dict(zip(mwLetterList, mwPageCount))
+    mwDict = dict(zip(letterList, pageCount))
 
     key: str
     for key in mwDict.keys():
@@ -71,8 +63,8 @@ if __name__ == "__main__":
             page: int
             for page in range(mwDict[key]):
                 pageString: str = str(page + 1)
-                html: BeautifulSoup = mw.getHTML(letter=key, page=pageString)
-                words += mw.getWords(html=html)
+                html: BeautifulSoup = getHTML(letter=key, page=pageString)
+                words += getWords(html=html)
                 mwDict[key] = len(words)
                 bar.next()
 
